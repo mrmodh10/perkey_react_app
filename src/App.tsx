004ffb8b211ef383f4edf6c5b84bbf7a5c040100
mohit <mohitmodh10@gmail.com>
2025-08-13 22:42:08 -0400

    few changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import InstagramLogin from "./pages/InstagramLogin";
 import VerifiedScreen from "./components/VerifiedScreen";
+import HomeScreen from "./pages/HomeScreen";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<InstagramLogin />} />
-      <Route path="/instagramLogin" element={<VerifiedScreen />} />
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/instagramLogin" element={<InstagramLogin />} />
+      <Route path="/verified" element={<VerifiedScreen />} />
     </Routes>
   );
 }
