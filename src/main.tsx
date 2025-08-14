fb0f52178dda8ed8c381982ed99013f20aaa9241
mohit <mohitmodh10@gmail.com>
2025-08-13 20:51:59 -0400

    made route change

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {createHashRouter, createRoutesFromElements , Route , RouterProvider } from 'react-router-dom';
+import Verified from './components/VerifiedScreen';
+import Home from './components/LoadingScreen';
 
-const container = document.getElementById("root");
-if (container) {
-  const root = ReactDOM.createRoot(container);
-  root.render(
-    <Router>
-      <App />
-    </Router>
-  );
-}
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const route = createHashRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path="/Verified" element={<Verified />} />
+      <Route path="/" element={<Home />} />
+    </Route>
+  )
+)
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={route}/>
+  </React.StrictMode>
+);
